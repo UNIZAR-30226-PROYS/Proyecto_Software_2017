6a0b5b5ff84422bb85d72f49a3612314bef56ffb
zory95 <rubencalatorao@gmail.com>
2017-04-09 23:55:09 +0200

    favoritos v3

--- java/04-06_LibrosFavoritos.java
+++ java/04-06_LibrosFavoritos.java
@@ -59,6 +59,51 @@ public class LibrosFavoritos extends AppCompatActivity {
     private void fillData(){
 
 
+        List<Row> rows = new ArrayList<Row>(30);
+        Row row = null;
+
+        rows.add(new Row("El Código da Vinci", "Dan Brown", "",(long) 0));
+        rows.add(new Row("Ángeles y Demonios", "Dan Brown", "", (long) 1));
+        rows.add(new Row("El símbolo perdido", "Dan Brown", "", (long) 2));
+        rows.add(new Row("El último deseo", "Andrzej Sapkowski", "", (long) 4));
+        rows.add(new Row("Memorias de Idhún I", "Laura Gallego","", (long) 4));
+        rows.add(new Row("La sombra del viento", "Carlos Ruiz Zafón", "",(long) 0));
+        rows.add(new Row("El testamento maya", "Steve Alten", "", (long) 1));
+        rows.add(new Row("Inferno", "Dan Brown", "", (long) 2));
+        rows.add(new Row("La brújula dorada", "Philip Pullman", "", (long) 4));
+        rows.add(new Row("Crpúsculo", "Stephanie Meyers", "", (long) 4));
+
+
+        if (!rows.isEmpty()){
+            TextView empty = (TextView) findViewById(R.id.empty);
+            empty.setWidth(0);
+        }
+
+        CustomArrayAdapter c = new CustomArrayAdapter(this, rows){
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+
+                if (button.isActivated()){
+                    button.setBackgroundResource(R.drawable.ic_slide_switch_on);
+                    Toast.makeText(getApplicationContext(), "Guardado como favorito",
+                            Toast.LENGTH_SHORT).show();
+                    button.setActivated(false);
+                }
+                else{
+                    button.setBackgroundResource(R.drawable.ic_slide_switch_off);
+
+                    Toast.makeText(getApplicationContext(), "Favorito borrado",
+                            Toast.LENGTH_SHORT).show();
+                    button.setActivated(true);
+                }
+            }
+        };
+
+        c.setIds(R.layout.activity_libros_favoritos, R.id.ciudadLibroF, R.id.nombreLibroF, R.id.usuarioLibroF, R.id.distLibroF, R.id.botonFav);
+
+        mList.setAdapter(c);
+
 
 
     }
