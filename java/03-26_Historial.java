ad43e634e28c7a999d77d9cd5c0dfe59c2684094
LauraOliva <702756@unizar.es>
2017-04-09 19:14:19 +0200

    Historial modificado

---
+++ java/03-26_Historial.java
@@ -0,0 +1,63 @@
+package barbarahliskov.cambialibros;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Historial extends AppCompatActivity {
+
+    private ListView mList;
+
+    /**
+     * Called when the activity is first created.
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        Button closeButton = (Button) findViewById(R.id.botonCerrar);
+
+        closeButton.setVisibility(View.INVISIBLE);
+        mList = (ListView)findViewById(R.id.list);
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // ListView Clicked item index
+                long itemPosition     = id;
+
+                // ListView Clicked item value
+                //int  itemValue    = mList.getitem
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(), "Hiciste click en el número " + itemPosition,
+                        Toast.LENGTH_LONG).show();
+                //closeButton.setText(Long.toString(itemPosition));
+            }
+        });
+
+
+        fillData();
+    }
+
+
+    private void fillData(){
+       
+
+    }
+
+    
+}
