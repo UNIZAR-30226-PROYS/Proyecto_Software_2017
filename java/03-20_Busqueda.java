ecb1505c61e32efce34b9d3300eb54f1c4e250c0
LegenDanny24 <danielmartinserrano@gmail.com>
2017-04-09 19:36:08 +0200

    Primera versión de Búsqueda

---
+++ java/03-20_Busqueda.java
@@ -0,0 +1,49 @@
+package barbarahliskov.cambialibros;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Busqueda extends AppCompatActivity {
+
+
+
+
+
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busqueda);
+        setTitle("Búsqueda");
+
+
+
+        
+
+       
+
+        
+ 
+
+
+    }
+
+
+}
