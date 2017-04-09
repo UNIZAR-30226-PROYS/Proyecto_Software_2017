ed18d3c6cbc72f62d81b2a04507ebb5f816461ca
LegenDanny24 <danielmartinserrano@gmail.com>
2017-04-09 19:50:14 +0200

    Primera versión de Menú

---
+++ java/03-20_Menu.java
@@ -0,0 +1,25 @@
+package barbarahliskov.cambialibros;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Menu extends AppCompatActivity {
+
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+
+
+}
