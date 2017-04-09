b169fec67d09ae2b0ba0adf806d48a4f270195e5
LauraOliva <702756@unizar.es>
2017-04-09 19:11:13 +0200

    ButtonList modificado

--- java/03-24_CustomArrayAdapter.java
+++ java/03-24_CustomArrayAdapter.java
@@ -17,6 +17,29 @@ import android.widget.Toast;
 public class CustomArrayAdapter extends ArrayAdapter<Row> implements
         View.OnClickListener {
 
-        
+        private LayoutInflater layoutInflater;
+        private int layout;
+        private int idAutor;
+        private int idTitulo;
+        private int idUsuario;
+        private int idDist;
+        private int idBoton;
+
+        public CustomArrayAdapter(Context context, List < Row > objects) {
+            super(context, 0, objects);
+            layoutInflater = LayoutInflater.from(context);
+        }
+
+        public void setIds (int layout, int a, int t, int u, int d, int b){
+            this.layout = layout;
+            this.idAutor = a;
+            this.idTitulo = t;
+            this.idUsuario = u;
+            this.idDist = d;
+            this.idBoton = b;
+        }
+    
+    
+
     
 }
