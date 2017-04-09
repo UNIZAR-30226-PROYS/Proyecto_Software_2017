27b7c142a39e44f8389a563aa0a754382e57679f
LauraOliva <702756@unizar.es>
2017-04-09 19:08:39 +0200

    CustomArrayAdapter empezado

---
+++ java/03-24_CustomArrayAdapter.java
@@ -0,0 +1,22 @@
+package barbarahliskov.cambialibros;
+
+import java.util.List;
+
+import barbarahliskov.cambialibros.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class CustomArrayAdapter extends ArrayAdapter<Row> implements
+        View.OnClickListener {
+
+        
+    
+}
