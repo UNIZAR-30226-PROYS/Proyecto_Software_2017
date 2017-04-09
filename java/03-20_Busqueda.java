9ce3d14e2a4f7502d94073989bec0c776fb719a1
LegenDanny24 <danielmartinserrano@gmail.com>
2017-04-09 19:37:45 +0200

    Segunda versión de Búsqueda: Botones localizacion

--- java/03-20_Busqueda.java
+++ java/03-20_Busqueda.java
@@ -35,7 +35,51 @@ public class Busqueda extends AppCompatActivity {
 
 
 
-        
+        /* Invisibiliza los campos aún no decididos */
+        EditText ciudad = (EditText) findViewById(R.id.ciudad);
+        ciudad.setVisibility(View.INVISIBLE);
+        EditText lat = (EditText) findViewById(R.id.latitud);
+        lat.setVisibility(View.INVISIBLE);
+        EditText lon = (EditText) findViewById(R.id.longitud);
+        lon.setVisibility(View.INVISIBLE);
+
+
+
+
+        /* Prepara el botón de Ciudad para visibilizar los campos
+           a rellenar, en caso de que se seleccione.
+         */
+        final RadioButton ciudadBtn = (RadioButton) findViewById(R.id.radio_ciudad);
+        final RadioButton coordBtn = (RadioButton) findViewById(R.id.radio_coord);
+        ciudadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText ciudad = (EditText) findViewById(R.id.ciudad);
+                ciudad.setVisibility(View.VISIBLE);
+                EditText lat = (EditText) findViewById(R.id.latitud);
+                lat.setVisibility(View.INVISIBLE);
+                EditText lon = (EditText) findViewById(R.id.longitud);
+                lon.setVisibility(View.INVISIBLE);
+                coordBtn.setChecked(false);
+            }
+        });
+
+        /* Prepara el botón de Coordenadas para visibilizar los campos
+           a rellenar, en caso de que se seleccione.
+         */
+
+        coordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText ciudad = (EditText) findViewById(R.id.ciudad);
+                ciudad.setVisibility(View.INVISIBLE);
+                EditText lat = (EditText) findViewById(R.id.latitud);
+                lat.setVisibility(View.VISIBLE);
+                EditText lon = (EditText) findViewById(R.id.longitud);
+                lon.setVisibility(View.VISIBLE);
+                ciudadBtn.setChecked(false);
+            }
+        });
 
        
 
