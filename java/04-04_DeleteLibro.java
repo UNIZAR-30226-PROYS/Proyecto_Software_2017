be105ddd6c21ab687f7f7e433b5b66ad635e00d8
698976 <698976@unizar.es>
2017-04-10 15:46:27 +0200

    Version 2 eliminar libro

--- java/04-04_DeleteLibro.java
+++ java/04-04_DeleteLibro.java
@@ -14,7 +14,9 @@ import java.util.List;
 
 public class DeleteLibro extends AppCompatActivity {
 
-
+    private ListView mList;
+    List<Row> rows = new ArrayList<Row>(30);
+    Row row = null;
 
     /**
      * Called when the activity is first created.
@@ -27,13 +29,49 @@ public class DeleteLibro extends AppCompatActivity {
 
         setContentView(R.layout.activity_list);
         setTitle("Libros favoritos");
-        
+        Button closeButton = (Button) findViewById(R.id.botonCerrar);
+
+        closeButton.setVisibility(View.INVISIBLE);
+        mList = (ListView) findViewById(R.id.list);
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // ListView Clicked item index
+                long itemPosition = id;
+
+
+
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(), "Se borrara el elemento " + rows.get( (int) itemPosition).getTitulo(),
+                        Toast.LENGTH_LONG).show();
+                // Intent i = new Intent(LibrosFavoritos.this, Busqueda.class);
+                // startActivity(i);
+            }
+        });
+
+
+
+
+        fillData();
+
+
 
 
     }
 
 
+    private void fillData() {
 
+
+
+
+
+
+    }
 
 
 
