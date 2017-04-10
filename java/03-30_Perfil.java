bc54260dfec454c8a7ae932a57ff6c0da78a4693
698976 <698976@unizar.es>
2017-04-10 15:48:56 +0200

    Version 1 perfil

---
+++ java/03-30_Perfil.java
@@ -0,0 +1,35 @@
+package barbarahliskov.cambialibros;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+/**
+ * Created by Jorge on 24/03/2017.
+ */
+
+public class Perfil extends AppCompatActivity{
+
+   
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Mi Perfil");
+
+        setContentView(R.layout.activity_perfil);
+
+
+    }
+
+    
+
+    
+}
