fd617d338a353b94a24dc72f0cf699db7ecdcbac
yolanda-96 <yolandasanchez1996@gmail.com>
2017-05-11 17:30:33 +0200

    VO modificados

--- Conectividad/VO/Lista_libros.java
+++ Conectividad/VO/Lista_libros.java
@@ -6,9 +6,9 @@ import javax.persistence.*;
 @Table(name = "Lista_libros")
 public class Lista_libros implements Serializable {
 	@Id
-	@Column(name = "id_list_libros",nullable=false)
+	@Column(name = "id_lista_libros",nullable=false)
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	private int id_list_libros;
+	private int id_lista_libros;
 	
 	@Column(name = "id_libro",nullable=false)
 	private int id_libro;
@@ -21,17 +21,17 @@ public class Lista_libros implements Serializable {
 	
 	public Lista_libros(int id_list_libros, int id_libro, Usuarios nickname) {
 		super();
-		this.id_list_libros = id_list_libros;
+		this.id_lista_libros = id_list_libros;
 		this.id_libro = id_libro;
 		this.nickname = nickname;
 	}
 
-	public int getId_list_libros() {
-		return id_list_libros;
+	public int getId_lista_libros() {
+		return id_lista_libros;
 	}
 
-	public void setId_list_libros(int id_list_libros) {
-		this.id_list_libros = id_list_libros;
+	public void setId_lista_libros(int id_list_libros) {
+		this.id_lista_libros = id_list_libros;
 	}
 
 	public int getId_libro() {
