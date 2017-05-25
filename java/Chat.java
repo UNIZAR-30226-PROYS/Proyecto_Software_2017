efeeebc761830c645d0d00b5a4e910a8eeabd2bf
LegenDanny24 <danielmartinserrano@gmail.com>
2017-05-25 18:56:50 +0200

    Version intermedia (o a lo mejor final) del chat

--- java/Chat.java
+++ java/Chat.java
@@ -11,16 +11,19 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -28,9 +31,27 @@ import android.widget.TableLayout;
 import android.widget.TextView;
 
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Chat extends AppCompatActivity {
 
@@ -42,6 +63,11 @@ public class Chat extends AppCompatActivity {
     private String pass;
     private String user1;
 
+    int escritor[] = new int[4];
+
+    ArrayList<String> parametros = new ArrayList<String>();
+
+    private String message = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +92,7 @@ public class Chat extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        setTitle("Chat con <LEGENDANNY>");
+        setTitle("Chat con " + user1);
         if(numMensajes == 0){
             LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
             LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
@@ -82,27 +108,9 @@ public class Chat extends AppCompatActivity {
         butSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(numMensajes == 0){
-                    LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
-                    layout1.setVisibility(View.VISIBLE);
-                    TextView texto = (TextView) findViewById(R.id.textView);
-                    texto.setText("Hola! Soy Daniel. Estoy interesado en tus ultimos dos libros, porque me gusta mucho Dan Brown. ¿Podríamos quedar para itnercambiarlos?");
-                    numMensajes++;
-                } else if(numMensajes == 1){
-                    LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
-                    layout2.setVisibility(View.VISIBLE);
-                    TextView texto = (TextView) findViewById(R.id.textView1);
-                    texto.setText("Si claro, ¿Qué día quieres quedar?");
-                    numMensajes++;
-                } else if(numMensajes == 2){
-                    LinearLayout layout3 = (LinearLayout) findViewById(R.id.layout3);
-                    layout3.setVisibility(View.VISIBLE);
-                    TextView texto = (TextView) findViewById(R.id.textView3);
-                    texto.setText("Me iría bien la semana que viene. \n No obstante, igual no puedo acudir porque tengo viajes de negocio. \n SI te parece bien, te aviso en cuanto pueda. \n De todas maneras, envíame tus coordenadas y así acudo ahí mismo.");
-                    numMensajes++;
-                }
-
-
+                EditText mMessage = (EditText) findViewById(R.id.Typemsgarea);
+                message = mMessage.getText().toString();
+                new InsertMessageTask().execute();
             }
         });
 
@@ -116,10 +124,10 @@ public class Chat extends AppCompatActivity {
             }
         });
 
-
-
+        new SearchMessagesReceivedTask().execute();
 
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -127,14 +135,233 @@ public class Chat extends AppCompatActivity {
         if (requestCode == 2) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
-
+                /*
                 LinearLayout layout4 = (LinearLayout) findViewById(R.id.layout4);
                 layout4.setVisibility(View.VISIBLE);
                 TextView texto = (TextView) findViewById(R.id.textView5);
                 texto.setText("Estas son mis coordenadas: " + data.getDataString());
-
-
+                */
             }
+        }
+    }
+
+
+
+    private class SearchMessagesReceivedTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... data) {
+
+            try {
+
+                String url_final = "http://10.0.2.2:8080/ChatCambiaLibros/SearchMessageServlet?";
+                url_final += "emisor=" + user1 + "&receptor=" + user;
+
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                URI website = new URI(url_final);
+                request.setURI(website);
+                HttpResponse response = httpClient.execute(request);
+
+
+
+                HttpEntity entity = response.getEntity();
+
+                // Read the contents of an entity and return it as a String.
+                String result = EntityUtils.toString(entity);
+                Log.d("url", url_final);
+
+                parametros = XML_Parser.parseaResultadoBusquedaMensajes(result);
+
+
+
+            } catch (MalformedURLException mue) {
+                mue.printStackTrace();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+
+            return "BIEN";
+
+        }
+
+        protected void onPostExecute(String page) {
+            /* Aqui tenemos los parametros */
+            int max = 4;
+            int numMensj = (parametros.size()/4);
+            if(numMensj<max) {
+                max = numMensj;
+            }
+
+                for(int i=0; i<max; i++){
+                    String id = parametros.get(4*i);
+                    String emisor = parametros.get(4*i+1);
+                    String receptor = parametros.get(4*i+2);
+                    String mensaje = parametros.get(4*i+3);
+
+                    if(i == 0){
+                        LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
+                        layout1.setVisibility(View.VISIBLE);
+                        TextView texto = (TextView) findViewById(R.id.textView1);
+                        texto.setText(mensaje);
+                        ImageView foto = (ImageView) findViewById(R.id.imageView12);
+                        foto.setBackgroundResource(R.drawable.user_pratikshya);
+                        escritor[numMensajes] = 0;        // 0 es el otro, 1 tu
+                        numMensajes++;
+                    } else if(i == 1){
+                        LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
+                        layout2.setVisibility(View.VISIBLE);
+                        TextView texto = (TextView) findViewById(R.id.textView2);
+                        texto.setText(mensaje);
+                        ImageView foto = (ImageView) findViewById(R.id.imageView22);
+                        foto.setBackgroundResource(R.drawable.user_pratikshya);
+                        escritor[numMensajes] = 0;        // 0 es el otro, 1 tu
+                        numMensajes++;
+                    } else if(i == 2) {
+                        LinearLayout layout3 = (LinearLayout) findViewById(R.id.layout3);
+                        layout3.setVisibility(View.VISIBLE);
+                        TextView texto = (TextView) findViewById(R.id.textView3);
+                        texto.setText(mensaje);
+                        ImageView foto = (ImageView) findViewById(R.id.imageView32);
+                        foto.setBackgroundResource(R.drawable.user_pratikshya);
+                        escritor[numMensajes] = 0;        // 0 es el otro, 1 tu
+                        numMensajes++;
+                    } else {
+                        LinearLayout layout4 = (LinearLayout) findViewById(R.id.layout4);
+                        layout4.setVisibility(View.VISIBLE);
+                        TextView texto = (TextView) findViewById(R.id.textView4);
+                        texto.setText(mensaje);
+                        ImageView foto = (ImageView) findViewById(R.id.imageView42);
+                        foto.setBackgroundResource(R.drawable.user_pratikshya);
+                        escritor[numMensajes] = 0;        // 0 es el otro, 1 tu
+                        numMensajes++;
+                    }
+                }
+
+        }
+
+    }
+
+    private class InsertMessageTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... data) {
+            // EN principio, el parametro llevara los datos
+            String text = "";
+            BufferedReader reader = null;
+
+            // Send data
+            try
+            {
+                String url = "http://10.0.2.2:8080/ChatCambiaLibros/" + "InsertMessageServlet";
+
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost request = new HttpPost(url);
+                List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+
+
+                postParameters.add(new BasicNameValuePair("emisor", user));
+                postParameters.add(new BasicNameValuePair("receptor", user1));
+                postParameters.add(new BasicNameValuePair("mensaje", message));
+
+
+                UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(
+                        postParameters);
+
+                request.setEntity(formEntity);
+                httpClient.execute(request);
+
+                // Defined URL  where to send data
+
+
+            } catch(Exception e) {
+                // Do something about exceptions
+                e.printStackTrace();
+            }
+
+            return "BIEN";
+
+        }
+
+        protected void onPostExecute(String page) {
+
+            if(numMensajes == 0){
+                LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
+                layout1.setVisibility(View.VISIBLE);
+                TextView texto = (TextView) findViewById(R.id.textView1);
+                texto.setText(message);
+                ImageView foto = (ImageView) findViewById(R.id.imageView12);
+                foto.setBackgroundResource(R.drawable.user_pacific);
+                escritor[numMensajes] = 1;        // 0 es el otro, 1 tu
+                numMensajes++;
+            } else if(numMensajes == 1){
+                LinearLayout layout2 = (LinearLayout) findViewById(R.id.layout2);
+                layout2.setVisibility(View.VISIBLE);
+                TextView texto = (TextView) findViewById(R.id.textView2);
+                texto.setText(message);
+                ImageView foto = (ImageView) findViewById(R.id.imageView22);
+                foto.setBackgroundResource(R.drawable.user_pacific);
+                escritor[numMensajes] = 1;        // 0 es el otro, 1 tu
+                numMensajes++;
+            } else if(numMensajes == 2){
+                LinearLayout layout3 = (LinearLayout) findViewById(R.id.layout3);
+                layout3.setVisibility(View.VISIBLE);
+                TextView texto = (TextView) findViewById(R.id.textView3);
+                texto.setText(message);
+                ImageView foto = (ImageView) findViewById(R.id.imageView32);
+                foto.setBackgroundResource(R.drawable.user_pacific);
+                escritor[numMensajes] = 1;        // 0 es el otro, 1 tu
+                numMensajes++;
+            } else if(numMensajes == 3) {
+                LinearLayout layout4 = (LinearLayout) findViewById(R.id.layout4);
+                layout4.setVisibility(View.VISIBLE);
+                TextView texto = (TextView) findViewById(R.id.textView4);
+                texto.setText(message);
+                ImageView foto = (ImageView) findViewById(R.id.imageView42);
+                foto.setBackgroundResource(R.drawable.user_pacific);
+                escritor[numMensajes] = 1;        // 0 es el otro, 1 tu
+                numMensajes++;
+            } else {
+                TextView texto1 = (TextView) findViewById(R.id.textView1);
+                TextView texto2 = (TextView) findViewById(R.id.textView2);
+                TextView texto3 = (TextView) findViewById(R.id.textView3);
+                TextView texto4 = (TextView) findViewById(R.id.textView4);
+                texto1.setText(texto2.getText());
+                texto2.setText(texto3.getText());
+                texto3.setText(texto4.getText());
+                texto4.setText(message);
+
+                if(escritor[1] == 1){
+                    escritor[0] = escritor[1];
+                    ImageView foto1 = (ImageView) findViewById(R.id.imageView12);
+                    foto1.setBackgroundResource(R.drawable.user_pacific);
+                } else {
+                    escritor[0] = escritor[1];
+                    ImageView foto1 = (ImageView) findViewById(R.id.imageView12);
+                    foto1.setBackgroundResource(R.drawable.user_pratikshya);
+                }
+
+                if(escritor[2] == 1){
+                    escritor[1] = escritor[2];
+                    ImageView foto2 = (ImageView) findViewById(R.id.imageView22);
+                    foto2.setBackgroundResource(R.drawable.user_pacific);
+                } else {
+                    escritor[1] = escritor[2];
+                    ImageView foto2 = (ImageView) findViewById(R.id.imageView22);
+                    foto2.setBackgroundResource(R.drawable.user_pratikshya);
+                }
+
+                if(escritor[3] == 1){
+                    escritor[2] = escritor[3];
+                    ImageView foto3 = (ImageView) findViewById(R.id.imageView32);
+                    foto3.setBackgroundResource(R.drawable.user_pacific);
+                } else {
+                    escritor[2] = escritor[3];
+                    ImageView foto3 = (ImageView) findViewById(R.id.imageView32);
+                    foto3.setBackgroundResource(R.drawable.user_pratikshya);
+                }
+
+                numMensajes++;
+            }
+
         }
     }
 
